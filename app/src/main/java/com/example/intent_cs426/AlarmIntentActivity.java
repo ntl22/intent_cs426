be263f07573d626ecb0ce849a9f02a6685b9c229
nhphucqt <hoangphuc.qti@gmail.com>
2023-06-18 00:20:02 +0700

    Created alarm intent

--- app/src/main/java/com/example/intent_cs426/AlarmIntentActivity.java
+++ app/src/main/java/com/example/intent_cs426/AlarmIntentActivity.java
@@ -2,7 +2,14 @@ package com.example.intent_cs426;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class AlarmIntentActivity extends AppCompatActivity {
 
@@ -10,5 +17,25 @@ public class AlarmIntentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_intent);
+
+        Button btn = findViewById(R.id.button_call_alarm_intent);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePicker timePicker = findViewById(R.id.alarm_timer_picker);
+                EditText alarmMessage = findViewById(R.id.input_alarm_message);
+                Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                        .putExtra(AlarmClock.EXTRA_MESSAGE, alarmMessage.getText().toString())
+                        .putExtra(AlarmClock.EXTRA_HOUR, timePicker.getHour())
+                        .putExtra(AlarmClock.EXTRA_MINUTES, timePicker.getMinute());
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Alarm onDestroy", "destroy");
     }
 }
