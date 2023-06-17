828d056ba9e5d8573750d8128ba6554738328779
nhphucqt <hoangphuc.qti@gmail.com>
2023-06-18 00:20:37 +0700

    Created

---
+++ app/src/main/java/com/example/intent_cs426/MusicVideoIntentActivity.java
@@ -0,0 +1,14 @@
+package com.example.intent_cs426;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicVideoIntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_video_intent);
+    }
+}
